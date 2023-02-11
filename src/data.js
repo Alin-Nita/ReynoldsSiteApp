8d424debfdb95a390196beb45ad690097a2d87b5
Alin Nita <mr.alin.nita@gmail.com>
2023-02-11 18:45:03 +0000

    changes about description

--- src/data.js
+++ src/data.js
@@ -3,7 +3,8 @@ export const sites = [
     id: 1,
     name: "Gwalia-Essar",
     adress: " London Road, Holyhead LL65 3DR",
-    description: "lorem ipsum lorem Gwalia",
+    description:
+      "site description, useful information regarding manoeuvring on site, informations regarding potting, hose lenght required, grids / inlets positioning, etc...all this to be filled by a competent person after assessing the site - ",
     image: [
       "./assets/pictures/Gwalia-Essar1.jpg",
       "./assets/pictures/Gwalia-Essar2.jpg",
@@ -14,7 +15,8 @@ export const sites = [
     id: 2,
     name: "Prysor-Essar",
     adress: "Trawsfynydd, Blaenau Ffestiniog LL41 4TW",
-    description: "lorem ipsum lorem Prysor",
+    description:
+      "site description, useful information regarding manoeuvring on site, informations regarding potting, hose lenght required, grids / inlets positioning, etc...all this to be filled by a competent person after assessing the site - ",
     image: [
       "./assets/pictures/Prysor-Essar1.jpg",
       "./assets/pictures/Prysor-Essar2.jpg",
@@ -25,7 +27,8 @@ export const sites = [
     id: 3,
     name: "Watson Fuels (Wigan)",
     adress: "57 Bickershaw Ln, Abram, Wigan WN2 5PL",
-    description: "lorem ipsum lorem Wigan",
+    description:
+      "site description, useful information regarding manoeuvring on site, informations regarding potting, hose lenght required, grids / inlets positioning, etc...all this to be filled by a competent person after assessing the site - ",
     image: [
       "./assets/pictures/Watson-FuelsWigan1.jpg",
       "./assets/pictures/Watson-FuelsWigan2.jpg",
