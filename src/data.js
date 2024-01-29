14324d4b98f0b42df124b9dcb5224c24a0f2be2b
Alin Nita <mr.alin.nita@gmail.com>
2024-01-29 11:48:17 +0000

    small changes4

--- src/data.js
+++ src/data.js
@@ -16,7 +16,7 @@ export const sites = [
     name: "Prysor-Essar",
     adress: "Trawsfynydd, Blaenau Ffestiniog LL41 4TW",
     description:
-      "site description,list of maneouvers to be performed and the specific order, useful information regarding manoeuvring on site, informations regarding potting, hose lenght required, grids / inlets positioning,potential hazards etc...all this to be filled by a competent person after assessing the site - ",
+      " - site description - list of maneouvers to be performed and the specific order - useful informations regarding manoeuvring on site -, informations regarding potting - , hose lenght required ** , grids / - inlets positioning - ,potential hazards * etc... - all this to be filled by a competent person after properly assessing the site",
     image: [
       "./assets/pictures/Prysor-Essar1.jpg",
       "./assets/pictures/Prysor-Essar2.jpg",
